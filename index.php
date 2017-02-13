47d9ebedabc6145700528464f51dc2ba7170d7c8
dtakkiy <dtakkiy00@gmail.com>
2017-02-13 22:12:00 +0900

    fixed index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
 echo "Hello GitHub!!";
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+	<title>Hello GitHub!!</title>
+<head>
+</head>
+<body>
+
+<ul>
+<li><a href="a.html">a</a></li>
+<li><a href="b.html">b</a></li>
+<li><a href="c.html">c</a></li>
+<li><a href="d.html">d</a></li>
+<li><a href="e.html">e</a></li>
+<li><a href="f.html">f</a></li>
+<li><a href="g.html">g</a></li>
+<li><a href="h.html">h</a></li>
+<li><a href="i.html">i</a></li>
+</ul>
+
+</body>
+</html>
